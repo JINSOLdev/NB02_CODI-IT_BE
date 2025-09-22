f6f20776251b67d7e029bfb064000c89a290b26c
JINSOLdev <130561876+JINSOLdev@users.noreply.github.com>
2025-09-22 17:15:00 +0900

    feat:s3업로드 기능 추가

---
+++ src/s3/s3.controller.ts
@@ -0,0 +1,29 @@
+import {
+  Controller,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+  Get,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { S3Service } from './s3.service';
+
+@Controller('api/s3')
+export class S3Controller {
+  constructor(private readonly s3Service: S3Service) {}
+
+  @Post('upload')
+	@UseInterceptors(FileInterceptor('file'))
+	async uploadImage(@UploadedFile() file: Express.Multer.File) {
+		const imageUrl = await this.s3Service.uploadFile(file);
+		return { imageUrl };
+	}
+
+	@Get('upload')
+	findAll() {
+		return {
+			message: 'success',
+		}
+	}
+	
+}
