e52ddc3ed5b65aacc8163d60c6d4ef87e0cd5492
JINSOLdev <130561876+JINSOLdev@users.noreply.github.com>
2025-09-22 11:26:10 +0900

    fix: 충돌 해결

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { StoreModule } from './store/store.module';
+import { CartsModule } from './cart/cart.module';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule, CartsModule, StoreModule],
   controllers: [AppController],
   providers: [AppService],
 })
