4d1263c31a29a4e9bbc4daa54b60096215816d23
chya-chya <cktndus32@naver.com>
2025-09-23 10:40:35 +0900

    feat:파일 유효성검증 & 크기제한

--- src/s3/s3.controller.ts
+++ src/s3/s3.controller.ts
@@ -3,19 +3,43 @@ import {
   Post,
   UploadedFile,
   UseInterceptors,
-  Get,
+  BadRequestException,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { S3Service } from './s3.service';
+
+export const imageFileFilter = (
+  req,
+  file: Express.Multer.File,
+  callback: (error: Error | null, acceptFile: boolean) => void,
+) => {
+  if (!file.mimetype.match(/\/(jpg|jpeg|png|gif)$/)) {
+    return callback(
+      new BadRequestException('Only image files are allowed!'),
+      false,
+    );
+  }
+  callback(null, true);
+};
 
 @Controller('api/s3')
 export class S3Controller {
   constructor(private readonly s3Service: S3Service) {}
 
   @Post('upload')
-  @UseInterceptors(FileInterceptor('file'))
+  @UseInterceptors(
+    FileInterceptor('file', {
+      fileFilter: imageFileFilter,
+      limits: {
+        fileSize: 1024 * 1024 * 10, // 파일 크기 제한 10MB
+      },
+    }),
+  )
   async uploadImage(@UploadedFile() file: Express.Multer.File) {
-    const imageUrl = await this.s3Service.uploadFile(file);
-    return { imageUrl };
+    if (!file) {
+      throw new BadRequestException('File is required');
+    }
+    const result = await this.s3Service.uploadFile(file);
+    return result;
   }
 }
