59e9d3d2609746242b747944dbc00e25c420ef39
JINSOLdev <130561876+JINSOLdev@users.noreply.github.com>
2025-09-25 17:22:56 +0900

    feat: 리뷰 업데이트 기능 추가 및 유효성 검사 구현

--- src/review/review.service.ts
+++ src/review/review.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
 import { CreateReviewDto } from './review.dto';
 import { ReviewRepository } from './review.repository';
 
@@ -8,5 +8,13 @@ export class ReviewService {
 
   create(createReviewDto: CreateReviewDto & { userId: string; productId: string }) {
     return this.reviewRepository.create({ ...createReviewDto });
+  }
+
+  async updateReview(userId: string, reviewId: string, rating?: number, content?: string) {
+    const existingReview = await this.reviewRepository.findReviewById(reviewId);
+    if (!existingReview) throw new NotFoundException('요청한 리소스를 찾을 수 없습니다.');
+    if (existingReview.userId !== userId) throw new UnauthorizedException('권한이 없습니다.');
+
+    return this.reviewRepository.updateReview(reviewId, rating, content);
   }
 }
