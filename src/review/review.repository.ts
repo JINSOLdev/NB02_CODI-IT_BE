9e49531b47ae74a93c4fef5741f98a99c5516f30
jrkgus413 <jrkgus413@naver.com>
2025-09-25 17:22:56 +0900

    feat: 리뷰 업데이트 기능 추가 및 유효성 검사 구현

--- src/review/review.repository.ts
+++ src/review/review.repository.ts
@@ -6,11 +6,24 @@ import { PrismaService } from 'src/prisma/prisma.service';
 export class ReviewRepository {
   constructor(private prisma: PrismaService) { }
 
+  findReviewById(reviewId: string) {
+    return this.prisma.review.findUnique({
+      where: { id: reviewId }
+    });
+  }
+
   create(createReviewDto: CreateReviewDto & { userId: string; productId: string }) {
     return this.prisma.review.create({
       data: { ...createReviewDto },
       select: { id: true, rating: true, content: true, userId: true, productId: true, createdAt: true },
     });
+  }
 
+  updateReview(reviewId: string, rating?: number, content?: string) {
+    return this.prisma.review.update({
+      where: { id: reviewId },
+      data: { rating, content },
+      select: { id: true, rating: true, content: true, userId: true, productId: true, createdAt: true },
+    });
   }
 }
