813f55888ba8a33fcaf5dfc185c842ac6b6fc0af
JINSOLdev <130561876+JINSOLdev@users.noreply.github.com>
2025-09-26 17:25:56 +0900

    feat: 리뷰 전체 / 상세 조회 기능 추가 및 검증 로직 구현

--- src/review/review.service.ts
+++ src/review/review.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
 import { CreateReviewDto } from './review.dto';
 import { ReviewRepository } from './review.repository';
 
@@ -6,7 +6,31 @@ import { ReviewRepository } from './review.repository';
 export class ReviewService {
   constructor(private reviewRepository: ReviewRepository) { }
 
+  // Review 등록
   create(createReviewDto: CreateReviewDto & { userId: string; productId: string }) {
     return this.reviewRepository.create({ ...createReviewDto });
+  }
+
+  // Product에 해당하는 Review 목록 조회
+  async findAllByProductId(userId: string, productId: string, query: { limit: number; page: number }) {
+    const product = await this.reviewRepository.findProductById(productId);
+    if (!product) throw new NotFoundException('요청한 리소스를 찾을 수 없습니다.');
+    return this.reviewRepository.findAllByProductId(productId, query);
+  }
+
+  // Review 단건 조회
+  async findReviewById(reviewId: string) {
+    const existingReview = await this.reviewRepository.findReviewById(reviewId);
+    if (!existingReview) throw new NotFoundException('요청한 리소스를 찾을 수 없습니다.');
+    return this.reviewRepository.findReviewById(reviewId);
+  }
+
+  // Review 수정
+  async updateReview(userId: string, reviewId: string, rating?: number, content?: string) {
+    const existingReview = await this.reviewRepository.findReviewById(reviewId);
+    if (!existingReview) throw new NotFoundException('요청한 리소스를 찾을 수 없습니다.');
+    if (existingReview.userId !== userId) throw new UnauthorizedException('권한이 없습니다.');
+
+    return this.reviewRepository.updateReview(reviewId, rating, content);
   }
 }
