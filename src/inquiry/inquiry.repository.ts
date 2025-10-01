481e3b77312f708568385989bf58a03044331c61
jrkgus413 <jrkgus413@naver.com>
2025-10-01 10:32:50 +0900

    refactor: 문의 목록 조회 기능 개선 및 코드 정리

--- src/inquiry/inquiry.repository.ts
+++ src/inquiry/inquiry.repository.ts
@@ -1,15 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { AnswerStatus } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
-import { GetInquiriesDto } from './inquiry.dto';
 
 @Injectable()
 export class InquiryRepository {
   constructor(private prisma: PrismaService) { }
 
   // 내가 작성한 문의 목록 조회
-  async getMyInquiries(userId: string, query: GetInquiriesDto) {
-    const { page = 1, pageSize = 16, status } = query;
-
+  async getMyInquiries(
+    userId: string,
+    page: number,
+    pageSize: number,
+    status?: AnswerStatus,
+  ) {
     // 트랜잭션으로 묶어서 리스트와 count 처리
     const result = await this.prisma.$transaction(async (tx) => {
       // 문의 리스트
@@ -18,10 +21,17 @@ export class InquiryRepository {
         skip: (page - 1) * pageSize,
         take: pageSize,
         select: {
-          id: true, title: true, isSecret: true, status: true, createdAt: true, content: true,
+          id: true,
+          title: true,
+          isSecret: true,
+          status: true,
+          createdAt: true,
+          content: true,
           product: {
             select: {
-              id: true, name: true, image: true,
+              id: true,
+              name: true,
+              image: true,
               store: { select: { id: true, name: true } },
             },
           },
