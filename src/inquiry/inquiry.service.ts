9704cc6779fe8998b5dd7fc609d1ddf1b08f058f
JINSOLdev <130561876+JINSOLdev@users.noreply.github.com>
2025-10-01 17:29:04 +0900

    feat: 문의 상세 조회 기능 추가

--- src/inquiry/inquiry.service.ts
+++ src/inquiry/inquiry.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InquiryRepository } from './inquiry.repository';
 import { GetInquiriesDto } from './inquiry.dto';
 
@@ -23,6 +23,23 @@ export class InquiryService {
     return {
       list: inquiries.list.map(formatInquiries),
       totalCount: inquiries.totalCount,
+    };
+  }
+
+  // 문의 상세 조회
+  async getInquiryDetail(inquiryId: string) {
+    const inquiry = await this.inquiryRepository.getInquiryById(inquiryId);
+    const reply = inquiry?.reply?.length ? inquiry.reply[0] : null;
+
+    // 문의가 존재하지 않거나 접근이 거부된 경우
+    if (!inquiry) throw new NotFoundException('문의가 존재하지 않습니다.');
+
+    return {
+      ...inquiry,
+      user: { name: inquiry.user.nickname },
+      // TODO : 추후 리팩토링 시 1:1 관계로 스키마 변경 예정
+      // 답변이 없을 경우를 대비한 옵셔널 체이닝
+      reply: reply ? { ...reply, user: { name: reply.user.nickname }, } : null,
     };
   }
 }
