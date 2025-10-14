3376980d93d41353d180e6d28ba68d3480da20a2
chya-chya <cktndus32@naver.com>
2025-10-14 15:07:00 +0900

    chorm:cart컨트롤러 스웨거 추가

--- src/cart/cart.dto.ts
+++ src/cart/cart.dto.ts
@@ -1,23 +1,30 @@
 import { IsArray, IsNumber, IsString, ValidateNested } from 'class-validator';
 import { Type } from 'class-transformer';
+import { ApiProperty } from '@nestjs/swagger';
+
 export class createCartDto {
   @IsString()
+  @ApiProperty({ example: 'user_id' })
   userId: string;
 }
 
 class SizeDto {
   @IsString()
+  @ApiProperty({ example: 'size_id' })
   sizeId: string;
 
   @IsNumber()
+  @ApiProperty({ example: 1 })
   quantity: number;
 }
 export class createOrUpdateCartItemsDto {
   @IsString()
+  @ApiProperty({ example: 'product_id' })
   productId: string;
 
   @IsArray()
   @ValidateNested({ each: true })
   @Type(() => SizeDto)
+  @ApiProperty({ type: [SizeDto] })
   sizes: SizeDto[];
 }
