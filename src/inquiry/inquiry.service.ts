e4c02ae5593444aa841a6005b4f20aa14f66e424
jrkgus413 <jrkgus413@naver.com>
2025-10-16 14:07:41 +0900

    refactor: 문의 목록 조회 및 사용자 정보 포맷 개선

--- src/inquiry/inquiry.service.ts
+++ src/inquiry/inquiry.service.ts
@@ -1,4 +1,9 @@
-import { ConflictException, Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { AnswerStatus } from '@prisma/client';
 import { InquiryRepository } from './inquiry.repository';
 import { GetInquiriesDto, ReplyContentDto, UpdateInquiryDto } from './inquiry.dto';
@@ -10,21 +15,7 @@ export class InquiryService {
   // 내가 작성한 문의 목록 조회
   async getMyInquiries(userId: string, query: GetInquiriesDto) {
     const { page = 1, pageSize = 16, status } = query;
-    const inquiries = await this.inquiryRepository.getMyInquiries(
-      userId,
-      page,
-      pageSize,
-      status,
-    );
-    const formatInquiries = ({ user, ...inquiry }) => ({
-      ...inquiry,
-      user: { name: user.nickname },
-    });
-
-    return {
-      list: inquiries.list.map(formatInquiries),
-      totalCount: inquiries.totalCount,
-    };
+    return await this.inquiryRepository.getMyInquiries(userId, page, pageSize, status);
   }
 
   // 문의 상세 조회
@@ -38,9 +29,9 @@ export class InquiryService {
 
     return {
       ...inquiry,
-      user: { name: inquiry.user.nickname },
+      user: { ...inquiry.user },
       // TODO : 추후 리팩토링 시 1:1 관계로 스키마 변경 예정
-      reply: reply ? { ...reply, user: { name: reply.user.nickname }, } : null,
+      reply: reply ? { ...reply } : null,
     };
   }
 
@@ -54,10 +45,12 @@ export class InquiryService {
     if (!inquiry) throw new NotFoundException('문의가 존재하지 않습니다.');
 
     // 내가 작성한 문의가 아닌 경우 접근 거부
-    if (inquiry.userId !== userId) throw new UnauthorizedException('자신이 작성한 문의만 수정할 수 있습니다.');
+    if (inquiry.userId !== userId)
+      throw new UnauthorizedException('자신이 작성한 문의만 수정할 수 있습니다.');
 
     // 답변이 이미 달린 경우 수정 불가(문의 상태가 답변 완료인 경우 || 답변이 이미 존재하는 경우)
-    if (reply || inquiry.status === AnswerStatus.CompletedAnswer) throw new ConflictException('답변이 이미 달린 문의는 수정할 수 없습니다.');
+    if (reply || inquiry.status === AnswerStatus.CompletedAnswer)
+      throw new ConflictException('답변이 이미 달린 문의는 수정할 수 없습니다.');
 
     return this.inquiryRepository.updateInquiry(inquiryId, title, content, isSecret);
   }
