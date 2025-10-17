943b97a170eecd80e84ca65c7a651f53809a8466
chya-chya <cktndus32@naver.com>
2025-10-17 10:41:36 +0900

    chorm:s3, 대쉬보드 스웨거 추가

--- src/dashboard/dashboard.controller.ts
+++ src/dashboard/dashboard.controller.ts
@@ -10,12 +10,27 @@ import { JwtAuthGuard } from 'src/auth/jwt.guard';
 import { AuthUser } from 'src/auth/auth.types';
 import { DashboardService } from './dashboard.service';
 import { UserType } from '@prisma/client';
+import { ApiOperation, ApiHeader, ApiResponse } from '@nestjs/swagger';
 
 @Controller('api/dashboard')
 export class DashboardController {
   constructor(private dashboardService: DashboardService) {}
 
   @UseGuards(JwtAuthGuard)
+  @ApiOperation({ summary: '대시보드 조회' })
+  @ApiHeader({
+    name: 'Authorization',
+    description: 'JWT 토큰',
+    required: true,
+  })
+  @ApiResponse({
+    status: 200,
+    description: '대시보드 조회 성공',
+  })
+  @ApiResponse({
+    status: 401,
+    description: '인증 실패',
+  })
   @Get()
   async getDashboard(@Req() req: { user: AuthUser }) {
     const user = req.user;
